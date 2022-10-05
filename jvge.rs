8f338ea2318d8a6d161049f5065edf19408fc360
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2022-10-05 09:30:42 +0000

    Add files via upload

---
+++ jvge.rs
@@ -0,0 +1,305 @@
+#![allow(dead_code, unused_variables, clippy::too_many_arguments, clippy::unnecessary_wraps)]
+
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::os::raw::c_void;
+
+use anyhow::{anyhow, Result};
+use log::*;
+use thiserror::Error;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::vk::ExtDebugUtilsExtension;
+
+//Whether Validation Layer should be enabled
+const VALIDATION_ENABLE: bool = cfg!(debug_assertions);
+//The name of the validation Layer
+const VALIDATION_LAYER: vk::ExtensionName = vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    //Window
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("jvge - vulkanalia")
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .build(&event_loop)?;
+
+    //App
+    let mut app = unsafe{ App::create(&window)? };
+    let mut destroying = false;
+    event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Poll;
+            match event {
+                //Render frame if app is not being destroyed.
+                Event::MainEventsCleared if !destroying => unsafe{ app.render(&window) }.unwrap(),
+                //Destroy the vulkan app.
+                Event::WindowEvent{event: WindowEvent::CloseRequested, .. } => {
+                        destroying = true;
+                        *control_flow = ControlFlow::Exit;
+                        unsafe{ app.destroy(); }
+                    }
+                _ => {}
+            }
+    
+
+        });
+}
+
+//The Vulkan App
+#[derive(Clone, Debug)]
+struct App {
+    entry: Entry,
+    instance: Instance,
+    data: AppData,
+    device: Device,
+}
+
+impl App {
+
+    //Create Vulkan App.
+    unsafe fn create(window: &Window) -> Result<Self> {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let mut data = AppData::default();
+        let instance = create_instance(window, &entry, &mut data)?;
+        pick_physical_device(&instance, &mut data)?;
+        let device = create_logical_device(&instance, &mut data)?;
+        Ok(Self{ entry, instance, data, device })
+    }
+
+    // Render a frame of the vulkan app
+    unsafe fn render(&mut self, window: &Window) -> Result <()> {
+        Ok(())
+    }
+
+    //Destroys out Vulkan app.
+    unsafe fn destroy(&mut self) {
+        if VALIDATION_ENABLE {
+            self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+        }
+        self.instance.destroy_instance(None);
+    }
+}
+
+//Vulkan handles and associated properties used by our Vulkan app.
+#[derive(Clone, Debug, Default)]
+struct AppData {
+    //Debug
+    messenger: vk::DebugUtilsMessengerEXT,
+    // Physical Device / Logical Device.
+    physical_device: vk::PhysicalDevice,
+
+    graphics_queue: vk::Queue,
+}
+
+
+//===========================================================
+//Instance
+//===========================================================
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
+    //Application Info
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"jvge - Vulkanalia\0")
+        .application_version(vk::make_version(1,0,0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1,0,0))
+        .api_version(vk::make_version(1,0,0));
+
+    //Available validation Layers
+    let available_layers = entry
+        .enumerate_instance_layer_properties()?
+        .iter()
+        .map(|l| l.layer_name)
+        .collect::<HashSet<_>>();
+
+    if VALIDATION_ENABLE && !available_layers.contains(&VALIDATION_LAYER) {
+        return Err(anyhow!("Validation layer requested but not supported."));
+    }
+
+    let layers = if VALIDATION_ENABLE {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        Vec::new()
+      };
+
+    //Extensions
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+        
+    if VALIDATION_ENABLE {
+        extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
+    }
+
+    //Create
+    let mut info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_layer_names(&extensions)
+        .enabled_extension_names(&extensions);
+
+    let mut debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+        .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+        .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+        .user_callback(Some(debug_callback));
+    if VALIDATION_ENABLE {
+        info = info.push_next(&mut debug_info);
+    }
+
+    let instance = entry.create_instance(&info, None)?;
+
+    //Messenger
+    if VALIDATION_ENABLE {
+        data.messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+    }
+
+    Ok(instance)
+}
+
+extern "system" fn debug_callback(severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+                                  type_: vk::DebugUtilsMessageTypeFlagsEXT,
+                                  data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+                                  _: *mut c_void) -> vk::Bool32 {
+
+    let data = unsafe{*data};
+    let message = unsafe{ CStr::from_ptr(data.message) }.to_string_lossy();
+
+    if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+        error!("({:?}) {}", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+        warn!("({:?} {})", type_, message);
+    } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+        debug!("({:?}) {}", type_, message);
+    } else {
+        trace!("({:?}) {}", type_, message);
+    }
+
+    vk::FALSE
+}
+
+//=======================================================
+// Physical Device
+//=======================================================
+#[derive(Debug, Error)]
+#[error("Missing {0}.")]
+pub struct SuitabilityError(pub &'static str);
+
+unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
+
+    for physical_device in instance.enumerate_physical_devices()? {
+        let properties = instance.get_physical_device_properties(physical_device);
+
+        if let Err(error) = check_physical_device(instance, data, physical_device) {
+            warn!("Skipping physical device (`{}`): {}", properties.device_name, error);
+        } else {
+            info!("Selected physical device (`{}`).", properties.device_name);
+            data.physical_device = physical_device;
+            return Ok(());
+        }
+
+    }    
+
+    Err(anyhow!("Failed to find suitable physical device"))
+}
+
+unsafe fn check_physical_device(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice) ->
+                                Result<()> {
+    QueueFamilyIndices::get(instance, data, physical_device)?;
+    Ok(())
+}
+
+
+//=======================================================
+// Logical Device
+//=======================================================
+unsafe fn create_logical_device(instance: &Instance, data: &mut AppData) -> Result<Device> {
+    //Queue Create Info's 
+    let indices = QueueFamilyIndices::get(instance, data , data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    //Layers
+    let layers = if VALIDATION_ENABLE {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        vec![]
+    };
+
+    //Features
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    //Create
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    //Queues 
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
+}
+
+
+//====================================================
+// Structs
+//====================================================
+#[derive(Copy, Clone, Debug)]
+struct QueueFamilyIndices {
+    graphics: u32
+}
+
+impl QueueFamilyIndices {
+    unsafe fn get(instance: &Instance, data: &AppData, physical_device: vk::PhysicalDevice) -> Result<Self> {
+        let properties = instance.get_physical_device_queue_family_properties(physical_device);
+
+        let graphics = properties
+            .iter()
+            .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
+            .map(|i| i as u32);
+
+        if let Some(graphics) = graphics {
+            Ok(Self {graphics})
+        } else {
+            Err(anyhow!(SuitabilityError("Missing required queue families.")))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
